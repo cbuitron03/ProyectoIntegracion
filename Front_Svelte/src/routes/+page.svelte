dc40433fa1a9b5f46ea9f3ab2350073fd69a3954
cbuitron03 <148727515+cbuitron03@users.noreply.github.com>
2025-07-03 02:01:43 -0500

    a

---
+++ Front_Svelte/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    goto('/home');
+  });
+</script>
+
+<p>Redirigiendo a Home...</p>
--- Front_Svelte/svelte.config.js
+++ Front_Svelte/svelte.config.js
@@ -1,8 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
-    // ...otras configuraciones
+    adapter: adapter({
+      out: 'build'
+    })
   }
 };
