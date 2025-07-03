1019766bad1001811964f59ad67f43dc7c613bc3
cbuitron03 <148727515+cbuitron03@users.noreply.github.com>
2025-07-03 03:08:31 -0500

    d

--- Front_Svelte/vite.config.js
+++ Front_Svelte/vite.config.js
@@ -1,11 +1,19 @@
 // vite.config.js
+// vite.config.js
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    host: true, // permite conexiones externas
-    allowedHosts: ['front-svelte.onrender.com'] // ← añade tu dominio aquí
+    host: '0.0.0.0',
+    strictPort: true,
+    port: process.env.PORT || 5173,
+    hmr: {
+      protocol: 'wss',
+      host: 'front-svelte.onrender.com',
+      clientPort: 443
+    },
+    allowedHosts: ['front-svelte.onrender.com']
   }
 });
