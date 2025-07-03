26dcc99830c555e51dff401ea2bf38af3c2d90f1
cbuitron03 <148727515+cbuitron03@users.noreply.github.com>
2025-07-03 02:51:27 -0500

    b

--- Front_Svelte/vite.config.js
+++ Front_Svelte/vite.config.js
@@ -1,7 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
+// vite.config.js
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    host: true, // permite conexiones externas
+    allowedHosts: ['https://front-svelte.onrender.com'] // ← añade tu dominio aquí
+  }
 });
