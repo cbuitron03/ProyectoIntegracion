1d7b6e68583b260c66db5723a57f1cf9e07282df
cbuitron03 <148727515+cbuitron03@users.noreply.github.com>
2025-07-03 02:26:26 -0500

    Revert "a"
    This reverts commit dc40433fa1a9b5f46ea9f3ab2350073fd69a3954.

--- Front_Svelte/svelte.config.js
+++ Front_Svelte/svelte.config.js
@@ -1,11 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
-  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      out: 'build'
-    })
+    adapter: adapter(),
+    // ...otras configuraciones
   }
 };
